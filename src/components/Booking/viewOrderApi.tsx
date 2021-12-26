e488643fd0d6cb8fd3a5faf65328dc1d1e399cfe
Seetha Rami Reddy N <60418966+seetharamireddyn@users.noreply.github.com>
2021-12-26 15:13:54 +0530

    fixed query param issue

--- src/components/Booking/viewOrderApi.tsx
+++ src/components/Booking/viewOrderApi.tsx
@@ -7,26 +7,8 @@ const url = "http://localhost:3214/booking";
 const ViewOrderApi = () => {
 
     const [booking, setBooking] = useState();
-    //const queryParams = new URLSearchParams(window.location.search)
 
     useEffect(() => {
-        // if(queryParams){
-        //     var data = {
-        //         "status":params.split('&')[1].split('=')[1],
-        //         "date":serachParam.split('&')[2].split('=')[1],
-        //         "bank":serachParam.split('&')[3].split('=')[1],
-        //     }
-        //     var id = serachParam.split('&')[1].split('=')[1].split('_')[1];
-        //     fetch(`${url}/${id}`,{
-        //         method:'PATCH',
-        //         headers: {
-        //             'Accept':'application/json',
-        //             'Content-Type':'application/json'
-        //         },
-        //         body: JSON.stringify(data)
-        //     })
-        // }
-
         axios.get(`${url}?email=nsr@gmail.com`)
             .then((res: any) => setBooking(res.data));
     })
